35d7385fb3aa06895d9522b4d63528da8b06a64b
bubbzDotDev <christopher.blount@outlook.com>
2022-04-07 12:12:54 -0500

    Credits and Home page updated

--- src/views/CreditsView.vue
+++ src/views/CreditsView.vue
@@ -3,6 +3,23 @@
     <h2>Attribution</h2>
     <ul>
       <li>
+        Special thanks to
+        <a
+          href="https://www.youtube.com/c/AnsontheDeveloper"
+          rel="noopener"
+          target="_blank"
+          >Anson The Developer</a
+        >: Anson's
+        <a
+          href="https://youtube.com/playlist?list=PL_cUvD4qzbkyX4Wp8TAfjpttjUldDWJnp"
+          rel="noopener"
+          target="_blank"
+          >2022 Discord Bot Dashboard Tutorial</a
+        >
+        got this project off the ground. Code from the tutorial kicked off the
+        creation of the backend API and Discord bot
+      </li>
+      <li>
         <a
           href="https://www.flaticon.com/free-icons/up-arrow"
           title="up arrow icons"
@@ -38,6 +55,10 @@ main {
   display: flex;
   flex-direction: column;
   align-items: center;
+}
+
+ul {
+  max-width: 500px;
 }
 
 li {
