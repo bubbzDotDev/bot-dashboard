1db31f263302b8ab099d7189cac771bc97e3a885
bubbzDotDev <christopher.blount@outlook.com>
2022-09-05 21:19:19 -0500

    Editing router

--- src/router/index.js
+++ src/router/index.js
@@ -14,7 +14,7 @@ import SettingsWelcome from "@/components/dashboard/guild/settings/SettingsWelco
 import AnnounceHome from "@/components/dashboard/guild/announce/AnnounceHome.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(`${import.meta.env.VITE_FRONTEND_HOST}`),
   routes: [
     {
       path: "/",
